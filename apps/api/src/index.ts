2eeff4fd925b0887b8812fc86be6cb672e6ef544
pablo <pfernandezrecaman@gmail.com>
2025-12-15 22:23:17 +0100

    new message from hono

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -2,7 +2,7 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => c.json({ message: 'Hello from API!' }))
+app.get('/', (c) => c.json({ message: 'Hello from Hono!' }))
 
 export default {
   port: 8000,
