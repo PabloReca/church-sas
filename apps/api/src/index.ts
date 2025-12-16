0ae6be8110eda5e21702e3471b3e47476ec24a90
pablo <pfernandezrecaman@gmail.com>
2025-12-16 13:27:03 +0100

    try deploy preprod

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -2,7 +2,7 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => c.json({ message: 'Hello from Hono!' }))
+app.get('/', (c) => c.json({ message: 'Hello from Hono.js!' }))
 
 export default {
   port: 8000,
